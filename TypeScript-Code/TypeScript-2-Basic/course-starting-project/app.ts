5a748d42f3447fc696c9dc945f026510a743c2d9
dnjsxoghd <feelchok1234@gmail.com>
2024-04-14 16:07:44 +0900

    arr and tuple

--- TypeScript-Code/TypeScript-2-Basic/course-starting-project/app.ts
+++ TypeScript-Code/TypeScript-2-Basic/course-starting-project/app.ts
@@ -1,8 +1,19 @@
-const person = {
+const person : {
+    name : string;
+    age : number;
+    hobbies : string [];
+    role : [number, string];
+} = {
   name: "Maxmilian",
   age: 30,
-  hobbies : ['Sports', 'Cooking']
+  hobbies : ['Sports', 'Cooking'],
+  role : [2, 'author']
 };
+
+// person.role.push('admin');
+// person.role[0] = 10;
+
+person.role =[0, 'adimin'];
 
 console.log(person.name);
 
