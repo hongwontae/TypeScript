1deadb21ac64d217fbb2cdaabeeb67bdc3328d7b
dnjsxoghd <feelchok1234@gmail.com>
2024-04-19 23:13:15 +0900

    TypeScript

--- TypeScript-Code/TypeScript-4-Class-Interface/cl-interfaces-01-starting-setup/src/app.ts
+++ TypeScript-Code/TypeScript-4-Class-Interface/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,9 +1,12 @@
 class Department {
-//   private name: string;
+// private name: string;
+// private readonly id : string 
   private employees : string[] = [];
 
-  constructor(private id: string, public name : string) {
-    
+
+  constructor(private readonly id: string, public name : string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this : Department){
@@ -11,6 +14,7 @@ class Department {
   }
 
   addEmployee(employee : string){
+    // this.id = '22' 불가능 => readonly이기 떄문에 바꿀 수 없다.
     this.employees.push(employee);
   }
 
@@ -21,12 +25,41 @@ class Department {
 
 }
 
+class ITDepartment extends Department{
+
+
+  constructor(id : string, public admins : string[]){
+    super(id, 'IT');
+    this.admins = admins
+  }
+}
+
+class AccountingDepartment extends Department{
+  constructor(id:string, private reports : string []){
+    super(id, 'Accounting');
+  }
+  addReports(text : string){
+    this.reports.push(text)
+  }
+
+
+  printResults(){
+    console.log(this.reports);
+  }
+}
+
 const accounting = new Department('D1',"Accounting");
 
 accounting.describe();
 accounting.addEmployee('Max');
 accounting.addEmployee('Lucas');
 accounting.printEmployeeInformation();
+1
+const IT= new ITDepartment('d1', ['Max']);
+console.dir(IT)
 
-// const accountCopy = {name : 's', describe : accounting.describe};
-// accountCopy.describe();
+const AccountTeam = new AccountingDepartment('A1',[]);
+AccountTeam.printResults();
+AccountTeam.addReports('Wrong!');
+AccountTeam.printResults();
+console.log(AccountTeam)
