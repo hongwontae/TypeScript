1122368dfbf1416799f82761ff5bba4e1c103736
dnjsxoghd <feelchok1234@gmail.com>
2024-04-21 21:23:40 +0900

    tommorow

--- TypeScript-Code/TypeScript-4-Class-Interface/cl-interfaces-01-starting-setup/src/app.ts
+++ TypeScript-Code/TypeScript-4-Class-Interface/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,17 +1,24 @@
-interface Person {
+interface Greetable {
     name : string;
-    age : number;
 
     greet(phrase : string) : void;
 };
 
-let user1 : Person;
-user1 = {
-    name : 'hwt',
-    age : 30,
-    greet(phrase : string ){
-        console.log(`${phrase} 입니다.`)
+class Person implements Greetable{
+    name: string;
+    age = 30;
+    constructor(n : string){
+        this.name = n;
     }
-};
+    greet(phrase: string): void {
+        console.log(phrase);
+    }
+    
+}
+
+let user1 : Greetable;
+
+user1 = new Person('HWT');
 
 user1.greet('HWT');
+console.log(user1)
