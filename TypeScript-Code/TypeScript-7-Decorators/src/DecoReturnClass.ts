9634c0ea219bed177ac202cb434d50159494f77e
dnjsxoghd <feelchok1234@gmail.com>
2024-04-27 17:49:41 +0900

    Decorator +

---
+++ TypeScript-Code/TypeScript-7-Decorators/src/DecoReturnClass.ts
@@ -0,0 +1,30 @@
+function h1Change(template : string, hookId : string){
+    return function<T extends {new(...args : any[]) : {name : string}}>(originalConstructor : T){
+        return class extends originalConstructor{   
+            constructor(..._ : any[]){
+                super();
+                const hookEl = document.getElementById(hookId);
+                if(hookEl){
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
+        }
+    }
+}
+
+@h1Change('<h1>Hello World</h1>','app')
+class Person2{
+    name = 'Max';
+
+    constructor(){
+        console.log('Creating person obj');
+    }
+
+}
+
+const pers2 = new Person2();
+console.log(pers2);
+
+
+
--- TypeScript-Code/TypeScript-7-Decorators/src/app.ts
+++ TypeScript-Code/TypeScript-7-Decorators/src/app.ts
@@ -1,30 +1,55 @@
-function h1Change(template : string, hookId : string){
-    return function<T extends {new(...args : any[]) : {name : string}}>(originalConstructor : T){
-        return class extends originalConstructor{   
-            constructor(..._ : any[]){
-                super();
-                const hookEl = document.getElementById(hookId);
-                if(hookEl){
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector('h1')!.textContent = this.name;
-                }
-            }
+function Log(target : any, propertyName : string){
+    console.log('Property decorator!')
+    console.log(target, propertyName);
+}
+
+function Log2(target : any, name : string, des : PropertyDescriptor){
+    console.log('Accessor decorator');
+    console.log(target);
+    console.log(name);
+    console.log(des)
+}
+
+function Log3(target : any, name : string, des : PropertyDescriptor){
+    console.log('Method!');
+    console.log(target);
+    console.log(name);
+    console.log(des)
+}
+
+function Log4(target : any, methodName : string | symbol, position : number){
+    console.log('Parameter!');
+    console.log(target)
+    console.log(methodName)
+    console.log(position)
+}
+
+class Product{
+    // @Log
+    title : string;
+    private _price :number;
+
+    // @Log2
+    public set price(v : number) {
+        if(v > 0){
+            this._price = v;
+        }else{
+            throw new Error('Fail');
         }
     }
-}
+    
 
-@h1Change('<h1>Hello World</h1>','app')
-class Person2{
-    name = 'Max';
+    constructor(t : string,p : number){
+        this.title = t;
+        this._price = p;
+    };
 
-    constructor(){
-        console.log('Creating person obj');
+    // @Log3
+    getPriceWithTax(@Log4 tax : number){
+        return this._price * (1 + tax);
     }
-
 }
 
-const pers2 = new Person2();
-console.log(pers2);
+const product = new Product('Hello-World', 1000);
 
-
-
+//DecoProp-Deco-gs-Deco-method-Deco-Parameter
