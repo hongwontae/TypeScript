1f4c6c36b3099b6565e271202fac1c6f8dfc90fa
dnjsxoghd <feelchok1234@gmail.com>
2024-04-30 15:49:08 +0900

    Generics review

--- TypeScript-Code/TypeScript-6-Generics/src/app.ts
+++ TypeScript-Code/TypeScript-6-Generics/src/app.ts
@@ -1,53 +1,54 @@
 // test
 
-const testA1 : Array<string> = ['1','2'];
+// const testA1 : Array<string> = ['1','2'];
 
-const promise : Promise<string> = new Promise((resolve, reject)=>{
-    if(1==1){
-        resolve('Resolve')
-    } else{
-        reject('Reject')
-    }
-});
+// const promise : Promise<string> = new Promise((resolve, reject)=>{
+//     if(1==1){
+//         resolve('Resolve')
+//     } else{
+//         reject('Reject')
+//     }
+// });
 
-promise.then(data => console.log(data.length));
+// promise.then(data => console.log(data.length));
 
-function add<T>(a : T){
-    return a;
-};
-const addResult = add<number>(10);
-console.log(addResult);
+// function add<T>(a : T){
+//     return a;
+// };
+// const addResult = add<number>(10);
+// console.log(addResult);
 
 
 
-function addPlus<T extends number , U extends string>(a: T, b : U){
-    let nData = a+Math.random();
-    let sData = b+' KoreaMan';
+// function addPlus<T extends number , U extends string>(a: T, b : U){
+//     let nData = a+Math.random();
+//     let sData = b+' KoreaMan';
 
-    let arr = [nData, sData];
-    return arr;
-}
+//     let arr = [nData, sData];
+//     return arr;
+// }
 
-const a = addPlus(4, 'UTube');
-console.log(a);
+// const a = addPlus(4, 'UTube');
+// console.log(a);
 
-interface CheckLength{
-    length : number;
-}
+// interface CheckLength{
+//     length : number;
+// }
 
-function lengthTypeCheck<T extends CheckLength >(a : T, b : T){
-    let AL = a.length;
-    let BL = b.length;
-    let com = AL+BL;
-    return com;
-};
+// function lengthTypeCheck<T extends CheckLength >(a : T, b : T){
+//     let AL = a.length;
+//     let BL = b.length;
+//     let com = AL+BL;
+//     return com;
+// };
 
-function okey<T extends object, U extends keyof T>(a : T, b : U){
-    return a[b];
-};
+// function okey<T extends object, U extends keyof T>(a : T, b : U){
+//     return a[b];
+// };
 
-const okey2 = okey({name : 'HWT'}, 'name');
-console.log(okey2)
+// const okey2 = okey({name : 'HWT'}, 'name');
+// console.log(okey2)
+
 
 
 
