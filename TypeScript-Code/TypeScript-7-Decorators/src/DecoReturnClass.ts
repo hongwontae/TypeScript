b4bcf7f0f3e5709f66fa0c331e0ca884a17905a8
dnjsxoghd <feelchok1234@gmail.com>
2024-05-01 17:04:35 +0900

    Decorator Review and return deco-Value

--- TypeScript-Code/TypeScript-7-Decorators/src/DecoReturnClass.ts
+++ TypeScript-Code/TypeScript-7-Decorators/src/DecoReturnClass.ts
@@ -1,30 +1,32 @@
-function h1Change(template : string, hookId : string){
-    return function<T extends {new(...args : any[]) : {name : string}}>(originalConstructor : T){
-        return class extends originalConstructor{   
-            constructor(..._ : any[]){
-                super();
-                const hookEl = document.getElementById(hookId);
-                if(hookEl){
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector('h1')!.textContent = this.name;
-                }
-            }
-        }
-    }
-}
+// function h1Change(template : string, hookId : string){
+//     return function<T extends {new(...args : any[]) : {name : string}}>(originalConstructor : T){
+//         return class extends originalConstructor{   
+//             constructor(..._ : any[]){
+//                 super();
+//                 const hookEl = document.getElementById(hookId);
+//                 if(hookEl){
+//                     hookEl.innerHTML = template;
+//                     hookEl.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
+//         }
+//     }
+// }
 
-@h1Change('<h1>Hello World</h1>','app')
-class Person2{
-    name = 'Max';
+// @h1Change('<h1>Hello World</h1>','app')
+// class Person2{
+//     name = 'Max';
 
-    constructor(){
-        console.log('Creating person obj');
-    }
+//     constructor(){
+//         console.log('Creating person obj');
+//     }
 
-}
+// }
 
-const pers2 = new Person2();
-console.log(pers2);
+// // const pers2 = new Person2();
+// // console.log(pers2);
+
+
 
 
 
--- TypeScript-Code/TypeScript-7-Decorators/src/Decorator-DecoratorOrder.ts
+++ TypeScript-Code/TypeScript-7-Decorators/src/Decorator-DecoratorOrder.ts
@@ -1,51 +1,51 @@
-// function Logger(logged : string){
-//     console.log('Logger Function')
-//     return function(constructor : Function){
-//         console.log(logged)
-//         console.dir(constructor);
-//     }
+// // function Logger(logged : string){
+// //     console.log('Logger Function')
+// //     return function(constructor : Function){
+// //         console.log(logged)
+// //         console.dir(constructor);
+// //     }
+// // }
+
+// // function h1Change(a : string, b : string){
+// //     console.log('h1-Change-Function')
+// //     return function(_ : Function){
+// //         console.log('h1Change')
+// //         const appDiv = document.querySelector(`#${b}`) as HTMLElement;
+// //         appDiv.innerHTML = a;
+// //     }
+// // }
+
+// // @Logger('Hello-World')
+// // @h1Change('<h1>Hello World</h1>','app')
+// // class Person{
+// //     name = 'Max';
+
+// //     constructor(){
+// //         console.log('Creating person obj');
+// //     }
+
+// // }
+
+// function Logger1(con1 : Function){
+//     console.log('Logger1 func')
+//     console.log(con1);
 // }
 
-// function h1Change(a : string, b : string){
-//     console.log('h1-Change-Function')
-//     return function(_ : Function){
-//         console.log('h1Change')
-//         const appDiv = document.querySelector(`#${b}`) as HTMLElement;
-//         appDiv.innerHTML = a;
-//     }
-// }
+// function Logger2(con2 : Function){
+//     console.log('Logger2 func')
+//     console.log(con2);
+// }   
 
-// @Logger('Hello-World')
-// @h1Change('<h1>Hello World</h1>','app')
-// class Person{
-//     name = 'Max';
+// @Logger1
+// @Logger2
+// class Person {
+//     name = 'HWT';
+//     age = 30;
 
 //     constructor(){
-//         console.log('Creating person obj');
+//         console.log('crating Person OBJ');
 //     }
-
 // }
-
-function Logger1(con1 : Function){
-    console.log('Logger1 func')
-    console.log(con1);
-}
-
-function Logger2(con2 : Function){
-    console.log('Logger2 func')
-    console.log(con2);
-}   
-
-@Logger1
-@Logger2
-class Person {
-    name = 'HWT';
-    age = 30;
-
-    constructor(){
-        console.log('crating Person OBJ');
-    }
-}
 
 
 
