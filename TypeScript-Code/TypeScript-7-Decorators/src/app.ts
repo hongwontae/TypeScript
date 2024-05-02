90d4f2d64bc401d6fc921e9bc76b35523f1374c7
dnjsxoghd <feelchok1234@gmail.com>
2024-05-02 15:16:25 +0900

    Decorator partial finish

--- TypeScript-Code/TypeScript-7-Decorators/src/app.ts
+++ TypeScript-Code/TypeScript-7-Decorators/src/app.ts
@@ -1,24 +1,45 @@
-class Printer{
-    message = 'This Works!'
+// class Printer{
+//     message = "This Works!"
+
+//     @AutoBind
+//     showMessage(){
+//         console.log(this.message);
+//     }
+// }
+
+// const p = new Printer();
+// const button = document.querySelector('button')! as HTMLElement;
+// button.addEventListener('click',p.showMessage);
+
+// function AutoBind(_1 : any, _2 : string, desc : PropertyDescriptor){
+//     const originalMethod = desc.value;
+//     const adjDescriptor : PropertyDescriptor = {
+//         configurable : true,
+//         enumerable : false,
+//         get(){
+//             const boundFn = originalMethod.bind(this);
+//             // 해당 게터 메서드는 언제나 자신이 속한 실제 객체에 의해 실행된다.
+//             return boundFn;
+//         }
+//     }
+//     return adjDescriptor;
+// }
+
+
+class Course {
+    @DeDeDecora
+    title = "TiTiTiTi"
 
     constructor(){
-        
+        console.log(this.title);
     }
-
-    printMe(){
-        console.log(this.message)
-    }
-
 }
 
-const p = new Printer();
-const pConsole = p.printMe.bind(p);
+function DeDeDecora(target: any, keyName: string){
+    let cool : string = keyName;
+    console.log(cool)
+    console.dir(target)
+};
 
-const button = document.querySelector('button')! as HTMLElement;
-button.addEventListener('click', function(){
-    pConsole();
-})
-
-function AutoBind(){
-    
-}
+const course = new Course();
+console.log(course)
